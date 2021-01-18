449e3cc5c4162d324b6810d32caba7bc38b1c5a4
Kellyn <38530508+Kellynhh@users.noreply.github.com>
2021-01-18 22:29:33 +0100

    Aporte a la interfaz
    Se añadió los primeros aportes para ir materializando el proyecto de Petex-Services
    Esta sujeto a cambios.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,81 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function App() {
-  return (
-    <div className="App">
-        <header>
-          hola
-        </header>
+
+    return (
         <div>
-          askdpsakpfksakdsak
+            <header>
+                <h1> Petex</h1>
+                <Nav fill variant="tabs" defaultActiveKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="/home">Inicio</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Quienes somos </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Contactanos </Nav.Link>
+                    </Nav.Item>
+
+
+                </Nav>
+            </header>
+            <body>
+            <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="...img/golden_cachorro.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+            </body>
+            <footer>
+
+            <h3>Contacta</h3>
+             <p>Pide tu cita</p>
+             <p>Pide tu presupuesto</p>
+
+            </footer>
+
         </div>
-        <input type="text"/>
-        {/* TU CODIGO AQUI  */}
-    </div>
-  );
-}
+    )
+};
 
 export default App;
